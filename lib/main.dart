83145b410a26cbd8c4fe0bc98151ef66fb76edc3
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-05-18 22:44:13 -0300

    Feat: Criando campo de digitação

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -39,15 +40,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: const Center(
-          child: Text('Hello World!'),
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: const IconThemeData(
+          color: Colors.blue,
         ),
       ),
+      home: const ChatScreen(),
     );
   }
 }
